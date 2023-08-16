48a43f0b8df12fcee3a9f55affd65957afd4ecb8
Your Name <you@example.com>
2023-08-16 11:56:55 +0530

    result view

---
+++ src/js/Views/resultView.js
@@ -0,0 +1,30 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg'; //parce (2)
+
+
+class resultView extends View {
+  _parentElement = document.querySelector('.results');
+
+  _errorMessage = 'No recipe foud for your query.Try again!';
+  _message = '';
+  _generatorMarkup() {
+    console.log(this._data);
+    return this._data.map(this._generatorMarkupPreview).join('');
+  }
+  _generatorMarkupPreview(result) {
+    return `
+     <li class="preview">
+            <a class="preview__link" href="#${result.id}">
+              <figure class="preview__fig">
+                <img src="${result.image}" alt="${result.title}" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${result.title}</h4>
+                <p class="preview__publisher">${result.publisher}</p>
+              </div>
+            </a>
+          </li>
+    `;
+  }
+}
+export default new resultView();
