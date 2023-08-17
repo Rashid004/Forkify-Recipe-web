c30793db8a3fcfc7ca4bb295b927e1ddaf3ce1c5
Your Name <you@example.com>
2023-08-18 00:31:42 +0530

    Adding Bookmarks

--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,16 @@
 
-import { async } from 'regenerator-runtime';
-import {API_URL} from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJson } from './helper.js';
 
 export const state = {
- recipe: {},
- search: {
- query: '',
- results: [],
- },
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function(id) {
@@ -27,7 +29,12 @@ try{
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(state.recipe);
+   if(state.bookmarks.some(bookmark => bookmark.id === id))
+   state.recipe.bookmarked = true
+  else state.recipe.bookmarked = false
+
+    state.search.page = 1
+    // console.log(state.recipe);
   }catch(err){
     console.error(`${err} 💥💥💥`);
     throw err;
@@ -40,17 +47,74 @@ export const loadSearchResults = async function(query) {
      const data = await getJson(`${API_URL}?search=${query}`);
 
    console.log(data);
-   state.search.results = data.data.recipes.map(rec => {
-    return {
-      id: rec.id,
-      title: rec.title,
-      publisher: rec.publisher,
-      image: rec.image_url,
-    }});
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+        // ...(rec.key && { key: rec.key }),
+      };
+    });
+    state.search.page = 1;
    
   }catch(err){
  console.error(`${err} 💥💥💥`);
  throw err;
   }
 };
-// loadSearchResults();
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; // 0
+  const end = page * state.search.resultsPerPage; // 9
+
+  return state.search.results.slice(start, end);
+};
+
+export const updateServings = function(newServings){
+state.recipe.ingredients.forEach(ing => {
+  ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  
+  console.log(newServings);
+});
+state.recipe.servings = newServings;
+};
+
+const persistBookmarks = function() {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+}
+
+
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmarked
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  persistBookmarks();
+};
+
+export const deleteBookmark = function (id) {
+  // Delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+  persistBookmarks();
+};
+
+const init = function() {
+const storage = localStorage.getItem('bookmarks');
+  if(storage) state.bookmarks = JSON.parse(storage);
+}
+init();
+
+const clear = function() {
+  localStorage.clear('bookmarks');
+};
+// clear();
