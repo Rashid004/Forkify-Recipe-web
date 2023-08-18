af501adba4999272e447d18505de778fd7c7a873
Your Name <you@example.com>
2023-08-18 17:07:43 +0530

    KEY and Etc

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,6 @@
 
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
-export const TIMEOUT_SEC = 10;
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10; 
+export const RES_PER_PAGE = 10;
+export const KEY = 'c2843195-28f7-406d-aa69-a85566fe6573';
+export const MODAL_CLOSEC_SEC = 2.5;
